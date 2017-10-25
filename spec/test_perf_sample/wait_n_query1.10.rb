bedc363527905c00026032b8b72840556a1df11f
Eric Kang <erickangatms@outlook.com>
2017-10-24 18:51:45 -0700

    removed perf template

--- spec/test_perf_sample/wait_n_query1.10.rb
+++
@@ -1,6 +0,0 @@
-describe 'WideWorldImporters: slow test query simulation' do
-
-    it 'waits and executes a query on a column without index' do
-        expect(sql.WideWorldImporters.test_perf_samples.wait_n_query.count).to be > 0
-    end
-end
